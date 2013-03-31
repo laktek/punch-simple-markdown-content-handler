da151b417504331a050587ebe71078cc8f2d1b38
Lakshan Perera <lakshan@web2media.net>
2013-03-31 15:53:17 +0530

    skeleton functions

---
+++ spec/simple_markdown_content_handler.spec.js
@@ -0,0 +1,24 @@
+var _ = require("underscore");
+var fs = require("fs");
+
+var simple_markdown_content_handler = require('../lib/simple_markdown_content_handler.js');
+
+describe("setup", function() {
+
+});
+
+describe("is section", function() {
+
+});
+
+describe("get sections", function() {
+
+});
+
+describe("negotiate content", function() {
+
+});
+
+describe("get content paths", function() {
+
+});
